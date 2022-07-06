6d6f7610198d88c37a500c8fa962c4e5b7c2609b
bilalkaya123 <84505170+bilalkaya123@users.noreply.github.com>
2022-07-06 17:07:16 +0300

    Sınıfkaldıgecti

---
+++ src/Giris/SinifKaldiGecti.java
@@ -0,0 +1,45 @@
+package Giris;
+import java.util.Scanner;
+public class SinifKaldiGecti {
+    public static void main(String[] args) {
+        double math,physics,turkish,chemistry,music;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter your math grade :");
+        math=sc.nextInt();
+        System.out.print("enter your physics grade :");
+        physics=sc.nextInt();
+        System.out.print("enter your turkish grade: ");
+        turkish=sc.nextInt();
+        System.out.println("enter your kimya grade : ");
+        chemistry=sc.nextInt();
+        System.out.println("enter your music grade : ");
+        music=sc.nextInt();
+
+
+        if(math<=0 || math>=100){
+            math=0;
+        }
+        if(physics<=0 || physics>=100){
+            physics=0;
+        }
+        if(chemistry<=0 || chemistry>=100){
+            chemistry=0;
+        }
+        if(turkish<=0 || turkish>=100){
+            turkish=0;
+        }
+        if(music<=0 || music>=100){
+            music=0;
+        }
+        double average= (math + physics + turkish + chemistry + music)/5;
+
+        if (average>=55){
+            System.out.println("You passed the class. Congratulations");
+        }else{
+            System.out.println("you failed the class. See you next year again");
+        }
+        System.out.println("Your Average: " + average);
+
+
+    }
+}
