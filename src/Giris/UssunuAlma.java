1f0a3517d52ce5bf7cf5795de70c7b4b9a580809
bilalkaya123 <84505170+bilalkaya123@users.noreply.github.com>
2022-07-08 08:51:39 +0300

    ussusayilar

--- src/Giris/UssunuAlma.java
+++ src/Giris/UssunuAlma.java
@@ -7,7 +7,11 @@ public class UssunuAlma {
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
 
-        for (int i=1;i<=n;i*=2){
+        for (int i=1;i<=n;i*=4){
+            System.out.println(i);
+        }
+        System.out.println("*****************************************");
+        for (int i=1;i<=n;i*=5){
             System.out.println(i);
         }
     }
