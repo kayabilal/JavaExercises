af0827599a009989d7eead95cea950c0250d12c8
kayabilal <kayabilal505@gmail.com>
2022-07-10 21:13:48 +0300

    TotalOfTheNumbers

---
+++ src/Giris/TotalOfTheNumbers.java
@@ -0,0 +1,18 @@
+package Giris;
+import java.util.Scanner;
+public class TotalOfTheNumbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        int total=0;
+        int x;
+
+        while(n!=0){
+            x=n%10;
+            total+=x;
+            n/=10;
+        }
+        System.out.println("total of the numbers "+total);
+    }
+}
