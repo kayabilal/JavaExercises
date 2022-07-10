938be0963d1e719fc90598d7023cc2bddcd1eb90
kayabilal <kayabilal505@gmail.com>
2022-07-10 22:44:39 +0300

    HarmonicNumbers

--- src/Giris/TotalOfTheNumbers.java
+++ src/Giris/TotalOfTheNumbers.java
@@ -2,17 +2,17 @@ package Giris;
 import java.util.Scanner;
 public class TotalOfTheNumbers {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter the number");
-        int n=sc.nextInt();
-        int total=0;
+        int n = sc.nextInt();
+        int total = 0;
         int x;
 
-        while(n!=0){
-            x=n%10;
-            total+=x;
-            n/=10;
+        while (n != 0) {
+            x = n % 10;
+            total += x;
+            n /= 10;
         }
-        System.out.println("total of the numbers "+total);
+        System.out.println("total of the numbers " + total);
     }
 }
