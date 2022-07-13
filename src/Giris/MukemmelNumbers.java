1ae19aee2ace81dbfd56533a6690de255851676f
kayabilal <kayabilal505@gmail.com>
2022-07-13 13:03:08 +0300

    MinMax

---
+++ src/Giris/MukemmelNumbers.java
@@ -0,0 +1,30 @@
+package Giris;
+import java.util.Scanner;
+public class MukemmelNumbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        while(true){
+            System.out.println("Enter a Number");
+            int number=sc.nextInt();
+            if(number==9){
+                break;
+            }
+            int total=0;
+            for(int i=1;i>number;i++){
+                if(number%i==0){
+                    total+=i;
+                }
+
+            }
+            if(total==number){
+                System.out.println("Mükemmel sayıdır");
+
+            }else{
+                System.out.println("Mükemmel sayi değildir.");
+            }
+
+        }
+
+    }
+}
