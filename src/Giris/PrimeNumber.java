4cb316c6ed3dd19af1aefc0ccac90916c330ce69
kayabilal <kayabilal505@gmail.com>
2022-07-13 13:32:15 +0300

    PrimeNumber

---
+++ src/Giris/PrimeNumber.java
@@ -0,0 +1,19 @@
+package Giris;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        for(int sayi=2;sayi<100;sayi++){
+            int kontrol=0;
+            for(int i=2;i<sayi;i++){
+                if(sayi%i==0){
+                    kontrol=1;
+                    break;
+                }
+
+            }
+            if(kontrol==0){
+                System.out.println(sayi+ "\n");
+            }
+        }
+    }
+}
